9bbb8c6a8e45dd8f2437666fcf95c3a83d5a58d8
zhaoxiaohua111 <xue2674671187@qq.com>
2021-03-22 16:49:56 +0800

    修改调试Account和Product

--- jpetstore-product/src/main/java/org/mybatis/jpetstore/product/JpetstoreProductApplication.java
+++ jpetstore-product/src/main/java/org/mybatis/jpetstore/product/JpetstoreProductApplication.java
@@ -5,12 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 
 import java.util.Locale;
@@ -18,30 +12,10 @@ import java.util.TimeZone;
 @EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("org.mybatis.jpetstore.product.persistence") // 扫描映射器
-public class JpetstoreProductApplication extends WebMvcConfigurerAdapter {
+public class JpetstoreProductApplication{
 
 	public static void main(String[] args) {
-		TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
 		SpringApplication.run(JpetstoreProductApplication.class, args);
 	}
 
-
-	@Bean
-	public LocaleResolver localeResolver() {
-		SessionLocaleResolver slr = new SessionLocaleResolver();
-		slr.setDefaultLocale(Locale.SIMPLIFIED_CHINESE);
-		return slr;
-	}
-
-	@Bean
-	public LocaleChangeInterceptor localeChangeInterceptor() {
-		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-		lci.setParamName("lang");
-		return lci;
-	}
-
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(localeChangeInterceptor());
-	}
 }
